9c263abd81e722437cac4b2cc281194461c87831
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-02-04 11:12:07 +0100

    add book feature to ltid

--- js/ltid.js
+++ js/ltid.js
@@ -1,13 +1,23 @@
 function changeLTID() {
     let oldid = getCookie("ltid", document.cookie);
+    let oldbook = getCookie("ltbook", document.cookie);
     let newid = prompt("LateinTrainerID eingeben:", oldid);
-    changeLTIDBackground(newid);
+    let newbook = prompt("Buch eingeben (CURSUS oder PRIMA):", oldbook);
+    if (newbook != "CURSUS") {
+        if (newbook != "PRIMA") {
+            alert("Invalides Buch!");
+            newbook = prompt("Buch eingeben (CURSUS oder PRIMA):", oldbook);
+        }
+    }
+    changeLTIDBackground(newid, newbook);
     document.getElementById("lttext__").textContent = "Einige Daten konnten eventuell noch nicht aktualisiert werden...";
-    document.getElementById("lttext__").textContent = newid;
+    document.getElementById("ltid__").textContent = newid;
+    document.getElementById("ltbook__").textContent = newbook;
 }
 
-async function changeLTIDBackground(newid) {
-    let response = await fetch("https://lkunited.pythonanywhere.com/ltp/setConnectedID?name="+getCookie("username", document.cookie)+"&code="+getCookie("code", document.cookie)+"&newid="+newid);
+async function changeLTIDBackground(newid, newbook) {
+    let response = await fetch("https://lkunited.pythonanywhere.com/ltp/setConnectedID?name="+getCookie("username", document.cookie)+\
+                               "&code="+getCookie("code", document.cookie)+"&newid="+newid+"&newbook="+newbook);
     let text = await response.text();
     alert("Änderung von Server beantwortet mit:"+text);
 }
@@ -16,8 +26,10 @@ async function getLTID() {
     let response = await fetch("https://lkunited.pythonanywhere.com/ltp/getConnectedID?name="+getCookie("username", document.cookie)+"&code="+getCookie("code", document.cookie));
     let text = await response.text();
     if (text != "Authentification failed") {
-        document.getElementById("ltid__").textContent = text;
-        setCookie("ltid", text, 7);
+        document.getElementById("ltid__").textContent = text.split("#*#")[0];
+        document.getElementById("ltbook__").textContent = text.split("#*#")[1];
+        setCookie("ltid", text.split("#*#")[0], 7);
+        setCookie("ltbook", text.split("#*#")[1], 7);
     } else {
         alert("Authentifizierung gescheitert!");
         document.getElementById("lttext__").textContent = "Fehler beim Abrufen";
