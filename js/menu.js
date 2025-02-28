f1da6224b5fd875e2f9a23720e5d2315cc09003c
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-02-28 17:27:00 +0100

    update menu

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 ﻿function changeMenuPointsLTP() {
     let menuTitlesShort = ["🏠", "👤", "🥇", "💻", "🔗", "☎"];
     let menuTitlesLong = ["Home", "Meine Daten", "Rangliste", "Desktop", "Weblinks", "Kontakt"];
-    renameMenuPointsAgent(menuTitlesShort, menuTitlesLong);
+    renameMenuPointsAgent(menuTitlesShort, menuTitlesLong, "rearrangedMenuLTP");
 }
